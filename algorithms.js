29f17a1b76ce4b6263dbf1cfc97e817759d06e80
kevinmalaverr <kevinmalaverr@gmail.com>
2021-04-18 14:07:22 -0500

    first commit

---
+++ algorithms.js
@@ -0,0 +1,16 @@
+export function bubble(arr) {
+  let len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      // this was missing
+      if (arr[j] > arr[j + 1]) {
+        // swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+import { bubble } from './algorithms.js';
+
+const HEIGHT_DIF = 20;
+
+const generateRandomNumbers = (length, min = 0) => {
+  const numbers = [];
+
+  for (let i = min; i < length + min; i++) {
+    numbers.push(i * HEIGHT_DIF);
+  }
+
+  numbers.sort(() => (Math.random() > 0.5 ? 1 : -1));
+
+  return numbers;
+};
+
+const makeNumberNodes = (numbers) => {
+  const nodes = [];
+
+  numbers.forEach((number) => {
+    const node = document.createElement('div');
+    node.className = 'bar';
+    node.style.height = `${number + 10}px`;
+    nodes.push(node);
+  });
+
+  return nodes;
+};
+
+const app = document.getElementById('app');
+
+const rand = generateRandomNumbers(10);
+
+console.log(bubble(rand));
+
+app.append(...makeNumberNodes(rand));
